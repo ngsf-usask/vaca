4f00bd39f82b5c17321134ef7ccdc5d67c62474e
Shahina Hayat (hxo752) <hxo752@platolgn01.cm.cluster>
2021-11-02 11:14:56 -0600

    modified:   mutect2_batch.sh 	modified:   run_sbatch

--- variants/gatk-mutect2-pipeline/mutect2_batch.sh
+++ variants/gatk-mutect2-pipeline/mutect2_batch.sh
@@ -12,19 +12,24 @@ set -eux
 
 #loading required modules
 module load gatk/4.2.2.0
+module load picard
 
 #reference file
 REF='/datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_assembly.genome.fa'
-
 PROJECT_ID='20-1LICH-001'
+INTERVALS=".bed"
 OUTDIR_NAME=$1
 BAM_FILE=$2
+
 
 #making project ID directory for storing results
 mkdir -p ${HOME}/projects/${PROJECT_ID}/
 mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}
 
-#Run Mutect2
-gatk Mutect2 -R ${REF} -I ${BAM_FILE} -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/unfiltered.vcf && gatk FilterMutectCalls -R ${REF} -V ${HOME}/projects/${PROJECT_ID}/${OUTDIR_NAME}/unfiltered.vcf -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/filtered.vcf
 
-#https://gatk.broadinstitute.org/hc/en-us/articles/360035531132--How-to-Call-somatic-mutations-using-GATK4-Mutect2
+#Run MarkDeduplication MarkDuplicates (https://gatk.broadinstitute.org/hc/en-us/articles/4405451219355-MarkDuplicatesSpark)
+#Run Mutect2 (https://gatk.broadinstitute.org/hc/en-us/articles/360035531132--How-to-Call-somatic-mutations-using-GATK4-Mutect2)
+
+java -jar $EBROOTPICARD/picard.jar MarkDuplicates I=input.bam --BARCODE_TAG="RX" O=${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/markduplicates.bam M=${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/marked_dup_metrics.txt && \
+gatk Mutect2 -R ${REF} -L ${INTERVALS} -I ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/markduplicates.bam -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/unfiltered.vcf && \
+gatk FilterMutectCalls -R ${REF} -V ${HOME}/projects/${PROJECT_ID}/${OUTDIR_NAME}/unfiltered.vcf -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/filtered.vcf
