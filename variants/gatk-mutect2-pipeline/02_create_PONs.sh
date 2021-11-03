f0c0f9c84935f628825920fbc56f4fd681c5405f
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-03 21:29:05 +0300

    Update 02_create_PONs.sh

--- variants/gatk-mutect2-pipeline/02_create_PONs.sh
+++ variants/gatk-mutect2-pipeline/02_create_PONs.sh
@@ -1,6 +1,6 @@
 #create Panel of Normals (PONs) using vcfs from previous step
 gatk CreateSomaticPanelOfNormals \
-    -vcfs ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz \
-    -vcfs 4_NA19771.vcf.gz \
-    -vcfs 5_HG02759.vcf.gz \
-    -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/pon.vcf.gz
+      -R ${REF} \
+      --germline-resource ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/af-only-gnomad.vcf.gz \
+      -V ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/pon_db \
+      -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/pon.vcf.gz
