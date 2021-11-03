e9ad0abc8911131e37d92c322b077137f210d3b4
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-03 21:34:19 +0300

    Update 02_create_GenomicsDB.sh

--- variants/gatk-mutect2-pipeline/02_create_GenomicsDB.sh
+++ variants/gatk-mutect2-pipeline/02_create_GenomicsDB.sh
@@ -1,5 +1,5 @@
 gatk GenomicsDBImport -R ${REF} \
-        --genomicsdb-workspace-path pon_db \
+        --genomicsdb-workspace-path ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/pon_db \
         -V normal1.vcf.gz \
         -V normal2.vcf.gz \
         -V normal3.vcf.gz
