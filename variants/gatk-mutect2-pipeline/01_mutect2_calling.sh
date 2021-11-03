dd7b7a258f3042db64eeeb471c610f06648bfd52
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-04 01:29:00 +0300

    Update and rename 01_mutect2_call_on_normal_samples.sh to 01_mutect2_calling.sh

--- variants/gatk-mutect2-pipeline/01_mutect2_calling.sh
+++ variants/gatk-mutect2-pipeline/01_mutect2_calling.sh
@@ -1,8 +1,7 @@
 #Run Mutect2 in tumor only mode (https://gatk.broadinstitute.org/hc/en-us/articles/360035531132--How-to-Call-somatic-mutations-using-GATK4-Mutect2
 #https://gatk.broadinstitute.org/hc/en-us/articles/360037593851-Mutect2)
-#First Run Mutect2 on normal samples to generate VCFs
+#Run Mutect2 on induced and uninduced samples to generate VCFs
 gatk Mutect2 \
      -R ${REF} \
-     --max-mnp-distance 0 \
      -I ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}_mdup_rg.bam \
      -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz
