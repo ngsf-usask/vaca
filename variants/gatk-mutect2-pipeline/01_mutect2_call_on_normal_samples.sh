06ca01bbf95ff609015a2e613a9dd6a0bb26d332
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-03 21:16:18 +0300

    Update 01_mutect2_call_on_normal_samples.sh

--- variants/gatk-mutect2-pipeline/01_mutect2_call_on_normal_samples.sh
+++ variants/gatk-mutect2-pipeline/01_mutect2_call_on_normal_samples.sh
@@ -3,5 +3,6 @@
 #First Run Mutect2 on normal samples to generate VCFs
 gatk Mutect2 \
      -R ${REF} \
+     --max-mnp-distance 0 \
      -I ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}_mdup_rg.bam \
      -O ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz
