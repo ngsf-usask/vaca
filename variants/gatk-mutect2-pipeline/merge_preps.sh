be61dc6ab654dbc3cf4968a21d688ded76d73d9d
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-04 12:08:08 -0600

    Update merge_preps.sh

--- variants/gatk-mutect2-pipeline/merge_preps.sh
+++ variants/gatk-mutect2-pipeline/merge_preps.sh
@@ -1,8 +1,27 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=samtools_merge
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1:30:00
+#SBATCH --mem=6G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
 
+#loading required module
+module load samtools
+
+#reference file
+PROJECT_ID='20-1LICH-001'
+PREP1=$1
+PREP2=$2 
+
+mkdir $
 # Merge preps into one bam file
 
-samtools merge -X ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}_mdup_rg.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${OUTDIR_NAME}/${OUTDIR_NAME}_mdup_rg.bam
+samtools merge -X ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP1}/${PREP1}_mdup_rg.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP2}/${PREP2}_mdup_rg.bam
 
 D2100001	E2100013
 D2100002	E2100014
@@ -16,3 +35,4 @@ D2100009	E2100021
 D2100010	E2100022
 D2100011	E2100023
 D2100012	E2100024
+PREP1
