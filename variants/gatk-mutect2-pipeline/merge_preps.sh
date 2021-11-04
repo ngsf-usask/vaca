8eec64515884657e3af713d9b8fbb4ac819abc37
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-04 13:26:40 -0600

    Update merge_preps.sh

--- variants/gatk-mutect2-pipeline/merge_preps.sh
+++ variants/gatk-mutect2-pipeline/merge_preps.sh
@@ -21,18 +21,7 @@ PREP2=$2
 mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam
 
 # Merge preps into one bam file
-samtools merge -X mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP1}/${PREP1}_mdup_rg.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP2}/${PREP2}_mdup_rg.bam
+samtools merge -X mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP1}/${PREP1}_mdup_rg.bam ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/${PREP2}/${PREP2}_mdup_rg.bam'
 
-D2100001	E2100013
-D2100002	E2100014
-D2100003	E2100015
-D2100004	E2100016
-D2100005	E2100017
-D2100006	E2100018
-D2100007	E2100019
-D2100008	E2100020
-D2100009	E2100021
-D2100010	E2100022
-D2100011	E2100023
-D2100012	E2100024
+while read -r LINE; do echo "$LINE"; sleep 0.5; done < test.txt
 
