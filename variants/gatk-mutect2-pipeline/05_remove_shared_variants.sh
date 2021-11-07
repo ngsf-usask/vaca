c6ec4944e69639f4af5b3fcf9c91cea1ab789bb4
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-07 03:20:38 +0300

    Update 05_remove_shared_variants.sh

--- variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
+++ variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=bcftools
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=2:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=4G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
@@ -17,14 +17,12 @@ module spider bcftools/1.13
 
 PROJECT_ID='20-1LICH-001'
 INPUT_DIR="${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/mutect2_calling"
-INDUCED_VCF=$1
-UNINDUCED_VCF=$2
+CLONE_ID=$1
+INDUCED_VCF=$2
+UNINDUCED_VCF=$3
 mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/filtered_vcfs/
 OUTPUT_DIR="${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/filtered_vcfs/"
 
-bcftools isec -C -c none -O z -o ${OUTPUT_DIR}${INDUCED_VCF}_${UNINDUCED_VCF} ${INDUCED_VCF} ${UNINDUCED_VCF}
+bcftools isec -C -c none -O z -o ${OUTPUT_DIR}${CLONE_ID}_${INDUCED_VCF}_${UNINDUCED_VCF}.vcf.gz ${INPUT_DIR}${INDUCED_VCF}.vcf.gz ${INPUT_DIR}${CLONE_ID}_${UNINDUCED_VCF}.vcf.gz
 
-bcftools merge --force-samples ${HOME}/projects/20-1LICH-001/mutect2-pipeline/mutect2_calling/D2100004_E2100016.vcf.gz ${HOME}/projects/20-1LICH-001/mutect2-pipeline/mutect2_calling/D2100009_E2100021.vcf.gz -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/filtered_vcfs/uninduced.vcf.gz -O z
-bcftools index -t ${HOME}/projects/20-1LICH-001/mutect2-pipeline/filtered_vcfs/uninduced.vcf.gz
-bcftools isec -C -c none -o ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/filtered_vcfs/induced.vcf.gz ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/mutect2_calling/D2100006_E2100018.vcf.gz ${HOME}/projects/20-1LICH-001/mutect2-pipeline/filtered_vcfs/uninduced.vcf.gz -O z
 
