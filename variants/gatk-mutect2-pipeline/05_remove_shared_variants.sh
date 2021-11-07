7a9c73befb2c23bfb21aa4cb8df05a34a7e896aa
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-07 03:54:19 +0300

    Update 05_remove_shared_variants.sh

--- variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
+++ variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
@@ -15,7 +15,7 @@
 #module load gcc/9.3.0
 #module spider bcftools/1.13
 
-#PROJECT_ID='20-1LICH-001'
+PROJECT_ID='20-1LICH-001'
 INPUT_DIR="${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/mutect2_calling/"
 CLONE_ID=$1
 PREP1=$2
@@ -24,6 +24,6 @@ PREP2=$3
 mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/filtered_vcfs/
 OUTPUT_DIR="${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/filtered_vcfs/"
 
-echo "bcftools isec -C -c none -O z -o ${OUTPUT_DIR}${CLONE_ID}_${PREP1}_${PREP2}.vcf.gz ${INPUT_DIR}${PREP1}_${PREP2}.vcf.gz ${INPUT_DIR}${CLONE_ID}_uninduced.vcf.gz" >> test.sh
+echo "bcftools isec -C -c none -O z -o ${OUTPUT_DIR}${CLONE_ID}_${PREP1}_${PREP2}.vcf.gz ${INPUT_DIR}${PREP1}_${PREP2}.vcf.gz ${INPUT_DIR}${CLONE_ID}_uninduced_merged.vcf.gz" >> test.sh
 
 
