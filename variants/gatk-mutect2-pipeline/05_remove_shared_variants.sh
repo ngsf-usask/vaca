88fef0715986ad86c46023056b5c9c82fef1f314
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-07 03:41:48 +0300

    Update 05_remove_shared_variants.sh

--- variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
+++ variants/gatk-mutect2-pipeline/05_remove_shared_variants.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+##!/bin/bash
 
-#SBATCH --account=hpc_p_anderson
-#SBATCH --constraint=skylake
-#SBATCH --job-name=bcftools
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time=30:00:00
-#SBATCH --mem=4G
-#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
-set -eux
+##SBATCH --account=hpc_p_anderson
+##SBATCH --constraint=skylake
+##SBATCH --job-name=bcftools
+##SBATCH --ntasks=1
+##SBATCH --cpus-per-task=2
+##SBATCH --time=30:00:00
+##SBATCH --mem=4G
+##SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+#set -eux
 
-#loading required modules
-module load StdEnv/2020
-module load gcc/9.3.0
-module spider bcftools/1.13
+##loading required modules
+#module load StdEnv/2020
+#module load gcc/9.3.0
+#module spider bcftools/1.13
 
-PROJECT_ID='20-1LICH-001'
+#PROJECT_ID='20-1LICH-001'
 INPUT_DIR="${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/mutect2_calling"
 CLONE_ID=$1
 PREP1=$2
