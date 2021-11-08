2e0f8758eb246d59f49f94d00f21ce5accf544c3
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-08 16:27:38 -0600

    Update 02_merge_preps.sh

--- variants/gatk-mutect2-pipeline/02_merge_preps.sh
+++ variants/gatk-mutect2-pipeline/02_merge_preps.sh
@@ -4,9 +4,9 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=samtools_merge
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=1:30:00
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+#SBATCH --time=1:00:00
+#SBATCH --mem=4G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
@@ -21,7 +21,7 @@ PREP2=$2
 mkdir -p ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam
 
 # Merge preps into one bam file
-#samtools merge -f ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam  /datastore/NGSF001/projects/20-1LICH-001/mutect2-pipeline/${PREP1}/${PREP1}_mdup_rg.bam /datastore/NGSF001/projects/20-1LICH-001/mutect2-pipeline/${PREP2}/${PREP2}_mdup_rg.bam
+samtools merge -f ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam  /datastore/NGSF001/projects/20-1LICH-001/mutect2-pipeline/${PREP1}/${PREP1}_mdup_rg.bam /datastore/NGSF001/projects/20-1LICH-001/mutect2-pipeline/${PREP2}/${PREP2}_mdup_rg.bam
 
 #index
 samtools index ${HOME}/projects/${PROJECT_ID}/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam
