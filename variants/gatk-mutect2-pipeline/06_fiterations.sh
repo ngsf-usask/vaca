7662629ab7619155744c0bde709569ed47523fe9
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-10 21:33:31 +0300

    Update 06_fiterations.sh

--- variants/gatk-mutect2-pipeline/06_fiterations.sh
+++ variants/gatk-mutect2-pipeline/06_fiterations.sh
@@ -1,8 +1,7 @@
  
  
  
-#bcftools query -f '%CHROM %POS %DP [%AF]\n' MCF7_A3A_uninduced_concat.vcf.gz |tail -n 50
-bcftools filter -i'FORMAT/DP>=15 && FORMAT/AF <=0.05' MCF7_A3A_uninduced_concat.vcf.gz | -l 
+#bcftools filter -i'FORMAT/DP>=15 && FORMAT/AF <=0.05' MCF7_A3A_uninduced_concat.vcf.gz -o MCF7_A3A_uninduced_filtered1.vcf.gz
 
 
-bcftools query -i'FORMAT/DP>=15 && AF <=0.05' -f'%CHROM %POS %REF %ALT %DP [%AF]\n' MCF7_A3A_uninduced_concat.vcf.gz -o MCF7_A3A_uninduced_filtered.vcf.gz
+bcftools query -i'FORMAT/DP>=15 && AF <=0.05' -f '%CHROM %POS %REF %ALT %DP [%AF] %TYPE\n' MCF7_A3A_uninduced_concat.vcf.gz -o MCF7_A3A_uninduced_filtered.vcf.gz
