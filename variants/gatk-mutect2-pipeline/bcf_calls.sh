58c5eb6549cbb0ee91dd2e51e2a5110067ff45f9
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-14 09:33:12 -0600

    Update bcf_calls.sh

--- variants/gatk-mutect2-pipeline/bcf_calls.sh
+++ variants/gatk-mutect2-pipeline/bcf_calls.sh
@@ -20,10 +20,11 @@ INTERVALS='/datastore/NGSF001/analysis/references/human/hg38/agilent_sureselect_
 PREP1=$1
 PREP2=$2
 
+mkdir -p ${INPUT_DIR}/bcf_mpileup
 #echo ${INPUT_DIR}/merged_bam/${PREP1}_${PREP2}_merged.bam >> ${PREP1}_${PREP2}.txt
 
-bcftools mpileup -Oz -f ${REF} ${HOME}/projects/20-1LICH-001/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}_mpileup.vcf.gz
-bcftools index -t ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}_mpileup.vcf.gz
+bcftools mpileup -Oz -f ${REF} {INPUT_DIR}/merged_bam/${PREP1}_${PREP2}_merged.bam -o ${INPUT_DIR}/bcf_mpileup/${PREP1}_${PREP2}_mpileup.vcf.gz
+bcftools index -t ${INPUT_DIR}/bcf_mpileup/${PREP1}_${PREP2}_mpileup.vcf.gz
 
 #bcftools call -mv -R ${INTERVALS} -f GQ -Oz -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}.vcf.gz
 #bcftools index -t ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}.vcf.gz
