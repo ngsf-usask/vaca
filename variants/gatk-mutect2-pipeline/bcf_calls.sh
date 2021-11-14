9f905486c283f29bd3cce55e407f273c6475ae24
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-14 09:31:46 -0600

    Update bcf_calls.sh

--- variants/gatk-mutect2-pipeline/bcf_calls.sh
+++ variants/gatk-mutect2-pipeline/bcf_calls.sh
@@ -22,6 +22,9 @@ PREP2=$2
 
 #echo ${INPUT_DIR}/merged_bam/${PREP1}_${PREP2}_merged.bam >> ${PREP1}_${PREP2}.txt
 
-bcftools mpileup -Oz -f ${REF} ${HOME}/projects/20-1LICH-001/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam | bcftools call -mv -R ${INTERVALS} -f GQ -Oz -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}.vcf.gz
+bcftools mpileup -Oz -f ${REF} ${HOME}/projects/20-1LICH-001/mutect2-pipeline/merged_bam/${PREP1}_${PREP2}_merged.bam -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}_mpileup.vcf.gz
+bcftools index -t ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}_mpileup.vcf.gz
+
+#bcftools call -mv -R ${INTERVALS} -f GQ -Oz -o ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}.vcf.gz
 #bcftools index -t ${HOME}/projects/20-1LICH-001/mutect2-pipeline/bcf_call/${PREP1}_${PREP2}.vcf.gz
 #https://speciationgenomics.github.io/filtering_vcfs/
