338fd7c8cedabd9cc0a99df8fa4c8a8554541c1f
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-16 13:12:07 -0600

    Update qulaity_check.R

--- variants/gatk-mutect2-pipeline/qulaity_check.R
+++ variants/gatk-mutect2-pipeline/qulaity_check.R
@@ -21,15 +21,17 @@ mutect2_var("MCF7_A3B_uninduced.txt")
 mutect2_var("MCF7_A3H_uninduced.txt")
 
 
-
+mcf7_var_lit <- read.table("/globalhome/hxo752/HPC/projects/20-1LICH-001/mutect2-pipeline/MCF7_variants_from_literature.txt", header=FALSE)
 setwd('/datastore/NGSF001/projects/20-1LICH-001/isomut2_results/')
 #Isomut2 variants
 isomut2_var <- function(sample){
-mcf7_var <- read.table(sample/all_SNVs.isomut2, header=FALSE)
-mcf7_var$V1 <- gsub("^chr","", as.character(mcf7_var$V1))
-merge_var <- merge(mcf7_var_lit,mcf7_var, by=c('V1','V2'))
+mcf7_var <- read.table(paste0(sample,"/all_SNVs.isomut2"), header=FALSE)
+mcf7_var$V2 <- gsub("^chr","", as.character(mcf7_var$V2))
+mcf7_var1 <- mcf7_var[c(2,3,6,7)]
+merge_var <- merge(mcf7_var_lit,mcf7_var1, by.x=c('V1','V2'), by.y=c('V2','V3'))
 #paste0(sample_file,"=", merge_var[!duplicated(merge_var$V2),])
 merge_var[!duplicated(merge_var$V2),]
+ merge_var
 }
 
 isomut2_var("E2100021")
