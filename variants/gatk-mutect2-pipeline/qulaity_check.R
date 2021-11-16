750a7c53a7e21c186ea5be7e489427d3a5ad606e
shayat <91986031+shhayat@users.noreply.github.com>
2021-11-16 11:20:18 -0600

    Create qulaity_check.R

---
+++ variants/gatk-mutect2-pipeline/qulaity_check.R
@@ -0,0 +1,12 @@
+mcf7_var_lit <- read.table("MCF7_variants_from_literature.txt", header=TRUE)
+
+mcf7_var <- read.table("MCF7_A3B_uninduced.txt", header=FALSE)
+mcf7_var$V1[grepl("chr",mcf7_var$V1)] <- ""
+
+mcf7_var_lit[(mcf7_var_lit$chromosome %in% mcf7_var$V1) && (mcf7_var_lit$position %in% mcf7_var$V2)]
+
+
+#SNVs extraction
+#bcftools view MCF7_A3B_uninduced_concat.vcf.gz -v snps -o MCF7_A3B_uninduced.vcf.gz
+#Required column Extraction
+#bcftools query -f '%CHROM %POS %REF %ALT\n' MCF7_A3B_uninduced.vcf.gz > MCF7_A3B_uninduced.txt
